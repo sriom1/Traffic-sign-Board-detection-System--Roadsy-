3bca98562f6962ce046079fe2fb4c84a3e61c9d2
srishanmukhaom <105510674+srishanmukhaom@users.noreply.github.com>
2024-03-25 11:39:27 +0530

    Update hand.py

--- hand.py
+++ hand.py
@@ -33,8 +33,8 @@ RTC_CONFIGURATION = RTCConfiguration(
 class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
-img = process(img)
-return av.VideoFrame.from_ndarray(img, format="bgr24")
+        img = process(img)
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 webrtc_ctx = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
